21276edac3b1c73d9d3f184dd15dbc33f9ea5124
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2024-06-02 19:34:24 +0300

    add tsconfigpaths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
