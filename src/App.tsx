115795b8f08d19e2fed814ce8f1930166e7b9748
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2024-06-02 19:39:49 +0300

    render todo route temporarily

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/reactQuery';
 
+import { Todo } from '@features/todo';
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>dashboard</div>
+      <Todo />
     </QueryClientProvider>
   );
 }
