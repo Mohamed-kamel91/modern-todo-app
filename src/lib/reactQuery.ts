8cb6ab0e8864dad932b2479a654f51a707e36785
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2024-06-30 19:32:16 +0300

    create lib for axios and react query

--- src/lib/reactQuery.ts
+++ src/lib/reactQuery.ts
@@ -1,3 +1,26 @@
-import { QueryClient } from '@tanstack/react-query';
+import { DefaultOptions, QueryClient, UseMutationOptions } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+const defaultOptions: DefaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 6, // 1 minute
+  },
+};
+
+export const queryClient = new QueryClient({ defaultOptions });
+
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<
+  ReturnType<T>,
+  'queryKey' | 'queryFn'
+>;
+
+export type ApiFnReturnType<FnType extends (...args: any) => Promise<any>> =
+  Awaited<ReturnType<FnType>>;
+
+export type MutationConfig<
+  MutationFnType extends (...args: any) => Promise<any>,
+> = UseMutationOptions<
+  ApiFnReturnType<MutationFnType>,
+  Error,
+  Parameters<MutationFnType>[0]
+>;
