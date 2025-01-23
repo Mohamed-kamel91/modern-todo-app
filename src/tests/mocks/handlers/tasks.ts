185acbbe6bdb1e6a358be9c98f8aea1448b20237
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-01-23 14:36:31 +0200

    modify get tasks api response and reduce network delay

--- src/tests/mocks/handlers/tasks.ts
+++ src/tests/mocks/handlers/tasks.ts
@@ -11,7 +11,7 @@ const env = import.meta.env;
 export const taskHandlers = [
   // Get tasks
   http.get(`${env.VITE_API_URL}/tasks`, async ({ request }) => {
-    await delay(1000);
+    await delay(50);
 
     const url = new URL(request.url);
     const userId = url.searchParams.get('userId');
@@ -23,25 +23,36 @@ export const taskHandlers = [
       );
     }
 
-    res.json(generateTasks());
+    const status = url.searchParams.get('status');
+    const tasks = generateTasks();
+    const filteredTasks =
+      status === 'active' || status === 'completed'
+        ? tasks.filter((task) => task.status === status)
+        : tasks;
+
+    return res.json(filteredTasks);
   }),
 
   // Create task
   http.post(`${env.VITE_API_URL}/tasks`, async ({ request }) => {
-    await delay(1000);
+    await delay(50);
+
     const data = (await request.json()) as Task;
-    res.json(data);
+    
+    return res.json(data);
   }),
 
   // Update task
   http.patch(
     `${env.VITE_API_URL}/tasks/:taskId`,
     async ({ request }) => {
-      await delay(1000);
+      await delay(50);
+
       const data = (await request.json()) as Partial<
         ReturnType<typeof generateTask>
       >;
-      res.json({ ...generateTask(), ...data });
+
+      return res.json({ ...generateTask(), ...data });
     }
   ),
 
@@ -49,9 +60,11 @@ export const taskHandlers = [
   http.delete(
     `${env.VITE_API_URL}/tasks/:taskId`,
     async ({ params }) => {
-      await delay(1000);
+      await delay(50);
+
       const taskId = params.taskId as string;
-      res.json({ ...generateTask(), id: taskId });
+
+      return res.json({ ...generateTask(), id: taskId });
     }
   ),
 ];
