cccb1e93809a220e87dcd141996a47a0db323e88
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-01-23 14:33:33 +0200

    clear query client after each test

--- src/tests/setup-tests.ts
+++ src/tests/setup-tests.ts
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/vitest';
 
 import { server } from './mocks/server';
+import { queryClient } from '@lib/reactQuery';
 
 beforeAll(() => {
   // Start the interception.
@@ -11,10 +12,10 @@ afterEach(() => {
   // Remove any handlers you may have added
   // in individual tests (runtime handlers).
   server.resetHandlers();
+  queryClient.clear();
 });
 
 afterAll(() => {
   // Disable request interception and clean up.
   server.close();
 });
-
