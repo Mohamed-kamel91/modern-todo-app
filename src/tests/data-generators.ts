a41f63d079731997257c631dbf693ed931f3e54a
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-01-23 14:35:37 +0200

    add more data generators utils

--- src/tests/data-generators.ts
+++ src/tests/data-generators.ts
@@ -1,5 +1,6 @@
 import { randTodo, randUuid } from '@ngneat/falso';
 
+import { User } from '@features/auth/types';
 import { Task } from '@features/tasks';
 
 export const generateTask = (): Task => ({
@@ -12,4 +13,22 @@ export const generateTask = (): Task => ({
   modifiedAt: new Date(),
 });
 
-export const generateTasks = (): Task[] => [generateTask()];
+export const createTask = (
+  overrides: Partial<ReturnType<typeof generateTask>> = {}
+): Task => ({
+  ...generateTask(),
+  ...overrides,
+});
+
+export const generateTasks = (): Task[] => [
+  createTask({ isCompleted: false, status: 'active' }),
+  createTask({ isCompleted: true, status: 'completed' }),
+];
+
+export const generateUser = (): User => ({
+  id: 'df425be7-9ad4-410a-b17e-6a696637e1fc',
+  firstName: 'mohamed',
+  lastName: 'kamel',
+  email: 'mohkamel91@gmail.com',
+  createdAt: new Date(),
+});
