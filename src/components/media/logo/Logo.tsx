47928f79cc38510d4cbd7dd282a92392262870ca
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-05-01 00:53:36 +0300

    changle logo name

--- src/components/media/logo/Logo.tsx
+++ src/components/media/logo/Logo.tsx
@@ -1,10 +1,10 @@
 export const Logo = () => {
   return (
     <div 
-      className="inline-block rounded-[30px] border border-dashed border-black px-[15px] py-[6px] text-[20px] font-bold"
+      className="inline-block rounded-[30px] border border-dashed border-black px-[15px] py-3 text-[20px] font-bold"
       aria-label="Todo Logo"
     >
-      Todo
+      ZapTasks
     </div>
   );
 };
